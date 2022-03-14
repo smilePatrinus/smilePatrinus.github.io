bdc3161c42cadd101e163dab29babd311ca4e135
smilePatrinus <80567865+smilePatrinus@users.noreply.github.com>
2022-03-14 22:21:49 +0800

    Update ChshFoodFunction.js

--- ChshFoodFunction.js
+++ ChshFoodFunction.js
@@ -1,9 +1,10 @@
 const foodName = [ "川辣子" ,"原山拉麵"];
 const foodImg = [
-    "https://pic.pimg.tw/fatchien33/1580797090-836611416_l.jpg","https://pic.pimg.tw/as660707/1617678181-51703685-g_wn.jpg"
+    "https://github.com/smilePatrinus/smilePatrinus.github.io/blob/main/P_20201219_202030_vHDR_Auto.jpg?raw=true","https://pic.pimg.tw/as660707/1617678181-51703685-g_wn.jpg"
 ];
 let randomNumber = 0;
 function Duckthis() {
+  /*  alert("製作中，敬請期待");*/
   randomNumber = Math.floor(Math.random() * 2);
   document.getElementById("food").innerHTML = foodName[randomNumber];
   document.getElementById("Foodimg").src = foodImg[randomNumber];
